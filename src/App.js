5517f49e57dcf825a71896297109d304368785f4
darakhsha rayen <darakhsharayen9@gmail.com>
2024-01-05 12:01:53 +0530

    latest code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+    <Navbar />  
+    <Main />
+    </>
+  );
+}
+
+export default App;
